bcef12d7dcd0a21b321070b4db9aa38817853c98
ysystudio <ysystudio@gmail.com>
2022-01-19 22:17:11 +0800

    add neovim another config with lua and window intstall scipt

---
+++ windows-install.sh
@@ -0,0 +1,56 @@
+# WARNING: PLEASE DO NOT USE THIS SCRIPT, JUST MANUALLY FOLLOW THE STEPS
+################# tools install#################
+choco install wget curl fzf lua lazygit 
+
+
+# ##############install zshrc ############################
+# ln -s ~/dev/dotfiles/zsh/zshrc ~/.zshrc
+# source ~/.zshrc
+
+# ############## link ranger###########################
+# ln -s ~/dev/dotfiles/ranger ~/.config/ranger
+# source ~/.config/ranger
+
+############## link lazygit###########################
+ln -s ~/dev/dotfiles/lazygit ~/.config/lazygit
+source ~/.config/lazygit
+
+############# install neovim ############################
+sudo port install neovim
+# gitui required by tui git operations
+# ripgrep required by telescope word search engine
+# ripgrep required by telescope-zoxide
+# sqlite required by telescope-frecency
+# fd required by telescope file search engine
+# yarn required by markdown preview
+# nerd-fonts-ibm-plex-mono required by devicons
+brew install git gitui zoxide ripgrep sqlite fd yarn 
+
+# nodejs, neovim-git required by copilot.
+# neovim version >= 0.6
+brew install nodejs neovim-git
+# for neovim python module
+pip install neovim --user
+
+# link config
+# ln -s ~/dev/dotfiles/nvim ~/.config/nvim
+ln -s ~/dev/dotfiles/nvim ~/AppData/Local/nvim
+# git clone https://github.com/ayamir/nvimdots ~/.config/nvim
+# cd ~/.local/share/nvim/site/pack/packer/opt/fzy-lua-native and make all to recompile
+# original bin for 10.15
+
+# sync plugins(maybe need multiple times)
+nvim +PackerSync
+#如果需要使用coc分支的：
+# cd ~/.config/nvim & git switch coc
+
+
+#################### alaritty - fastest terminal####################
+choco install alacritty
+# link the config
+ln -s ~/dev/dotfiles/alacritty ~/.config/alacritty
+
+#################### fonts###########################################
+# If you are using powerlevel10k, p10k configure can install the recommended font for you. 
+choco install FiraCode
+
