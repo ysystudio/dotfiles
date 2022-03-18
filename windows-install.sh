d384691c6752c1e84d29b33a177c9d7a01210e3a
ysystudio <ysystudio@gmail.com>
2022-03-18 17:55:53 +0800

    add choco install

--- windows-install.sh
+++ windows-install.sh
@@ -1,5 +1,14 @@
 # WARNING: PLEASE DO NOT USE THIS SCRIPT, JUST MANUALLY FOLLOW THE STEPS
 ################# tools install#################
+#install choco
+#cmd with admin
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+#powershell
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#test choco
+choco -v
+choco upgrade chocolatey
+
 choco install wget curl fzf lua lazygit 
 # alacritty 0.9.0
 # FiraCode 6.2
