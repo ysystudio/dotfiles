bb9db21131fc2aa41f04231dbf17f0d816619372
ysystudio <ysystudio@gmail.com>
2022-04-13 18:00:48 +0800

    update the install.sh of nvim-shark

--- nvim-shark/install.sh
+++ nvim-shark/install.sh
@@ -1,6 +1,6 @@
 # add the link
 
-ln -s ~/dev/dotfiles/nvim-shark/custom ~/dev/dotfiles/nvim-shark/NvChad/lua/custom
-ln -s ~/dev/dotfiles/nvim-shark/NvChad ~/.config/nvim
+ln -s ~/dev/dotfiles/nvim-shark/custom ~/dev/dotfiles/nvim-shark/nvchad/lua/custom
+ln -s ~/dev/dotfiles/nvim-shark/nvchad ~/.config/nvim
 
 # this version , the lsp server need install mannully, for example npm -g pyright
