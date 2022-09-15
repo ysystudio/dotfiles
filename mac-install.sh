5b7e5e188798c3312bb04640defde5e79d3b29d6
ysystudio <ysystudio@gmail.com>
2022-09-15 15:22:27 +0800

    add dev-sidecar and F9(project build and run

--- mac-install.sh
+++ mac-install.sh
@@ -51,3 +51,14 @@ brew install font-FiraCode-nerd-font
 brew install alacritty
 # link the config
 ln -s ~/dev/dotfiles/alacritty ~/.config/alacritty
+# dev sidecar install 
+https://github.com/docmirror/dev-sidecar/releases
+# turn on the power mode
+# change the seeting.json5 under ~/.dev-sidecar
+{
+  overwall: true,
+  installTime: 1663221978753,
+  rootCa: {
+    setuped: true,
+  },
+}
