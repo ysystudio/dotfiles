f911c06498b2ff16b4ff4095338ffede3c518c8c
ysystudio <ysystudio@gmail.com>
2022-10-31 09:58:03 +0000

    clean lvim install.sh

--- lvim/install.sh
+++ lvim/install.sh
@@ -1,14 +1,6 @@
-
-# config.lua
-# require("user-keymaps")
-# require("user-options")
-# require("user-plugins")
-
-# add the link
 unlink ~/.config/lvim
 ln -s ~/dev/dotfiles/lvim ~/.config/lvim
 
-# add the link for Asynctasks
 unlink ~/.vim/tasks.ini
 ln -s ~/dev/dotfiles/lvim/tasks.ini ~/.vim/tasks.ini
 
