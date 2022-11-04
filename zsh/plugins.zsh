f834fd39934ecf72c0a4b5212279300db8da6281
ysystudio <ysystudio@gmail.com>
2022-11-04 02:27:29 +0000

    recover p10k, because starship has issue on prompt

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,6 +2,8 @@
 source $ZI_HOME/zinit.zsh
 
 # if can not load , try to zinit delete OMZP::name and re do source .zshrc
+zinit depth=1 lucid nocd for \
+    romkatv/powerlevel10k
 
 zinit light paulirish/git-open
 zinit light zsh-users/zsh-autosuggestions
