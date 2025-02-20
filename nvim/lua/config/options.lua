79e5b552571592cf0844099cee607ef5f490cc5f
ysystudio <ysystudio@gmail.com>
2025-02-20 23:13:32 +0800

    update lazyvim config to nvim/

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
 
 -- 设置asynctasks窗口排版
 vim.g.asynctasks_term_pos = "tab"
@@ -9,3 +9,4 @@ vim.g.asynctasks_term_rows = 15
 vim.g.asynctasks_term_reuse = 1
 vim.g.asynctasks_term_focus = 1
 vim.g.asyncrun_open = 6
+
